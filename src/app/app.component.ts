2a7ada3be8bd7b2a1d40c1067478969db1a1814e
Emil Lehti <29.lehti@gmail.com>
2020-08-19 19:00:55 +0200

    More config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,9 @@ export class AppComponent implements OnInit{
 
   enableElectricCurtain: boolean;
   brewCoffee: boolean;
+  bar: string;
+  desc: string;
+  edit: string;
 
   constructor() {}
 
@@ -19,20 +22,31 @@ export class AppComponent implements OnInit{
     const factory = SplitFactory({
       core: {
         authorizationKey: splitioKey,
-        key: 'CUSTOMER_ID'
+        key: 'Publisher2',
+        trafficType: 'role'
      }
     });
 
     const client = factory.client();
 
     client.on(client.Event.SDK_READY, () => {
-      const brew = client.getTreatment('brew-coffee');
-      if (brew) {
-        this.brewCoffee = true;
+      this.brewCoffee = client.getTreatment('feature') === 'on';
+      const x = client.getTreatment('best_bar');
+      if (x === 'mitm') {
+        this.bar = 'Man in The Moon';
+        this.desc = 'God IPA';
+      } else if (x === 'grodan') {
+        this.bar = 'Grodan';
+        this.desc = 'Moretti på fat!';
+      } else if (x === 'erlands') {
+        this.bar = 'Erlands';
+        this.desc = 'Bees Knee`s från himmelen';
       } else {
-        this.brewCoffee = false;
+        this.bar = 'Dovas';
+        this.desc = 'Gå ej hit';
       }
-
+      this.edit = client.getTreatment('edit_button');
+      console.log(this.edit);
     });
   }
 }
